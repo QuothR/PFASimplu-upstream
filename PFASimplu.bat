6d2d9519a1f9d309b414df508f9be384703d0fbf
QuothR <radu.harabagiu@gmail.com>
2026-09-05 21:17:40 +0300

    Lansator: asteapta pana cand serverul raspunde si refoloseste un server deja pornit
    La pornirea la rece Django incarca pandas si matplotlib si dureaza mai mult
    de 3 secunde, iar browserul se deschidea inainte si primea connection refused.
    Acum lansatorul porneste serverul in fereastra lui, verifica cu curl pana la
    60 s si abia apoi deschide browserul. Daca serverul ruleaza deja, doar
    deschide browserul.

--- PFASimplu.bat
+++ PFASimplu.bat
@@ -1,6 +1,26 @@
 @echo off
 title PFASimplu
 cd /d "%~dp0"
-echo Pornesc PFASimplu... Inchide aceasta fereastra ca sa opresti aplicatia.
-start "" cmd /c "timeout /t 3 >nul & start http://127.0.0.1:8000"
-".venv\Scripts\python.exe" manage.py runserver 127.0.0.1:8000 --noreload
+
+rem Daca serverul ruleaza deja, doar deschide browserul.
+curl -s -o NUL http://127.0.0.1:8000 && goto open
+
+echo Pornesc PFASimplu...
+start "PFASimplu - inchide fereastra ca sa opresti aplicatia" ".venv\Scripts\python.exe" manage.py runserver 127.0.0.1:8000 --noreload
+
+rem Asteapta pana cand serverul raspunde (maxim 60 s).
+set /a n=0
+:wait
+set /a n+=1
+if %n% gtr 60 goto fail
+timeout /t 1 >nul
+curl -s -o NUL http://127.0.0.1:8000 && goto open
+goto wait
+
+:open
+start "" http://127.0.0.1:8000
+exit
+
+:fail
+echo Serverul nu a pornit in 60 de secunde. Verifica fereastra "PFASimplu".
+pause
