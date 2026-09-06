7f03a69ff9ce03cc7adf1e5f688ad814679535c8
QuothR <radu.harabagiu@gmail.com>
2026-09-06 13:48:02 +0300

    utils/valuta.py: inapoi la versiunea upstream, hostul BNR e reparat in cursvalutarbnr 1.0.6

--- utils/valuta.py
+++ utils/valuta.py
@@ -3,46 +3,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from cursvalutarbnr import ron_exchange_rate
 from functools import lru_cache
-import cursvalutarbnr
-import requests
-import xmltodict
-from decimal import Decimal, ROUND_HALF_UP
-
-
-# BNR a mutat XML-urile de curs de pe www.bnr.ro pe curs.bnr.ro (august 2026).
-# www.bnr.ro raspunde acum cu redirect 302 catre o pagina HTML, iar libraria
-# cursvalutarbnr 1.0.5 are hostul vechi scris in cod. Inlocuim functia de
-# descarcare cu una identica, dar cu hostul nou.
-BNR_HOST = "https://curs.bnr.ro"
-
-
-def _get_bnr_rates_for_year(year: int):
-    cache = True
-    url = f"{BNR_HOST}/files/xml/years/nbrfxrates{year}.xml"
-    r = requests.get(url)
-    bnr_ron_rates = xmltodict.parse(r.content)
-
-    if "Cube" not in bnr_ron_rates["DataSet"]["Body"]:
-        cache = False
-        r = requests.get(f"{BNR_HOST}/nbrfxrates10days.xml")
-        bnr_ron_rates = xmltodict.parse(r.content)
-
-    exchange_rates = {}
-    for entries in bnr_ron_rates["DataSet"]["Body"]["Cube"]:
-        rates = {}
-        for entry in entries["Rate"]:
-            currency = entry["@currency"]
-            value = Decimal(entry["#text"])
-            multiplier = Decimal(entry.get("@multiplier", "1"))
-            rates[currency] = float(
-                (value * multiplier).quantize(Decimal("0.0001"), rounding=ROUND_HALF_UP)
-            )
-        exchange_rates[entries["@date"]] = rates
-
-    return exchange_rates, cache
-
-
-cursvalutarbnr.get_bnr_rates_for_year = _get_bnr_rates_for_year
 
 
 @lru_cache(maxsize=None)
